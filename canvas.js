cd4180af6ed01505c20f55cdfb4d041c2fad35d7
flavia <flavia.e.arroyo@gmail.com>
2022-02-04 19:07:56 -0300

    proyecto funcionando

---
+++ canvas.js
@@ -0,0 +1,103 @@
+var pantalla = document.querySelector("#ahorcado")
+
+var pincel = pantalla.getContext("2d")
+pincel.fillStyle= "grey"
+
+function dibujarBase(){
+    pincel.beginPath();
+    pincel.moveTo(400, 350);
+    
+    pincel.lineTo(300, 350);
+    
+    pincel.lineTo(500,350)
+    
+    pincel.lineWidth = 15;
+    pincel.strokeStyle = "#000"
+    pincel.stroke()
+    pincel.closePath();
+
+
+}
+
+function dibujarPoste(){
+    
+    pincel.beginPath();
+    pincel.moveTo(150, 100);
+    pincel.lineTo(150, 50);
+    pincel.lineTo(400,50);    
+    pincel.lineTo(400, 350);
+    pincel.lineWidth = 15;
+    pincel.strokeStyle = "#000"
+    pincel.stroke()
+    pincel.closePath();
+
+}
+
+
+
+
+function dibujarRostro(){
+    pincel.beginPath();
+    pincel.arc(150,140,40,0,Math.PI * 2, false);
+    pincel.strokeStyle = "red";
+    pincel.lineWidth = 5;
+    pincel.stroke();
+    pincel.closePath();
+
+
+}
+
+function dibujarTorzo(){
+    pincel.beginPath();
+    pincel.moveTo(150,180);
+    pincel.lineTo(150,250);
+    
+    pincel.strokeStyle = "red";
+    pincel.lineWidth = 5;
+    pincel.stroke();
+    pincel.closePath()
+
+}
+
+function dibujarBrazos(){
+    pincel.beginPath();
+    pincel.moveTo(120,220);
+    pincel.lineTo(150,180);
+    pincel.lineTo(180,220)
+    pincel.strokeStyle = "red";
+    pincel.lineWidth = 5;
+    pincel.stroke();
+    pincel.closePath()
+
+}
+
+function dibujarPiernas(){
+    pincel.beginPath();
+    pincel.moveTo(120,290);
+    pincel.lineTo(150,250);
+    pincel.lineTo(180,290)
+    pincel.strokeStyle = "red";
+    pincel.lineWidth = 5;
+    pincel.stroke();
+    pincel.closePath()
+
+}
+
+function dibujarOjosMuertos(){
+    pincel.beginPath();
+    //ojo izquierdo
+    pincel.moveTo(125,120);
+    pincel.lineTo(145,145);
+    pincel.moveTo(145,120);
+    pincel.lineTo(125,145);
+    //ojo derecho
+    pincel.moveTo(155,120);
+    pincel.lineTo(175,145);
+    pincel.moveTo(175,120);
+    pincel.lineTo(155,145);
+
+    pincel.strokeStyle = "blue";
+    pincel.lineWidth = 5;
+    pincel.stroke();
+    pincel.closePath()
+}
